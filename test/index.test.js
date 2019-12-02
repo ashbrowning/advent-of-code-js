9f394dcdfcc33c1c46cfc0b46598144f528b0cda
Ash Browning <ambrowning89@gmail.com>
2019-12-02 21:56:56 +0000

    feat(esm): convert other parts over to ESM

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
-const { getInputPath, getSolutionPath } = require('../tools/pathResolvers');
-const fs = require('fs');
+import fs from 'fs';
+import { getInputPath, getSolutionPath } from '../tools/pathResolvers';
 
 describe("Advent of Code 2019 JS", () => {
   for (let i = 1; i <= 1; ++i) {
@@ -7,13 +7,13 @@ describe("Advent of Code 2019 JS", () => {
       const input = fs
       .readFileSync(getInputPath(i), { encoding: "utf8" })
       .split("\n");
-      it('should give the correct answers for part 1', () => {
-        const { solution, answer } = require(getSolutionPath(i, 1));
+      it('should give the correct answers for part 1', async () => {
+        const { solution, answer } = await import(getSolutionPath(i, 1));
 
         expect(solution(input)).toBe(answer);
       });
-      it('should give the correct answers for part 2', () => {
-        const { solution, answer } = require(getSolutionPath(i, 2));
+      it('should give the correct answers for part 2', async () => {
+        const { solution, answer } = await import(getSolutionPath(i, 2));
 
         expect(solution(input)).toBe(answer);      });
     });
