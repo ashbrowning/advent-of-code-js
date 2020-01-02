28b26c62bb139dfa76ee4d6bf072dfef73afcb7d
Ash Browning <ambrowning89@gmail.com>
2020-01-02 10:19:15 +0000

    refactor(day9): making int machine extendable

--- src/utils/intMachine.js
+++ src/utils/intMachine.js
@@ -1,35 +1,44 @@
+const getOperand = (instructions, fullOpCode, ptr, paramIndex ) => {
+  const paramMode = parseInt(fullOpCode.slice(-3 - paramIndex, -2 - paramIndex) || 0, 10)
+  return paramMode ? instructions[ptr + paramIndex + 1] : instructions[instructions[ptr + paramIndex + 1]];
+}
+
+const setValue = (instructions, fullOpCode, ptr, paramIndex, value) => {
+  const paramMode = parseInt(fullOpCode.slice(-3 - paramIndex, -2 - paramIndex) || 0, 10);
+  if (paramMode === 2) {
+    console.log('paramode 2');
+  } else {
+    instructions[instructions[ptr + paramIndex + 1]] = value;
+  }
+};
 
 function* runIntMachine(instructionsParam, inputParams = []) {
   const outputs = [];
   const instructions = instructionsParam.slice();
   const inputs = inputParams.slice();
   let ptr = 0;
+  let relativeBase = 0;
 
   while (ptr < instructions.length) {
     const fullOpCode = '' + instructions[ptr];
     const opCode = parseInt(fullOpCode.slice(-2));
-    const operand1 =
-      parseInt(fullOpCode.slice(-3, -2)) || 0
-        ? instructions[ptr + 1]
-        : instructions[instructions[ptr + 1]];
-    const operand2 =
-      parseInt(fullOpCode.slice(-4, -3)) || 0
-        ? instructions[ptr + 2]
-        : instructions[instructions[ptr + 2]];
+    const operand1 = getOperand(instructions, fullOpCode, ptr, 0);
+    const operand2 = getOperand(instructions, fullOpCode, ptr, 1);
     switch (opCode) {
       case 1:
         // Addition
-        instructions[instructions[ptr + 3]] = operand1 + operand2;
+        setValue(instructions, fullOpCode, ptr, 2, operand1 + operand2);
+        // instructions[instructions[ptr + 3]] = operand1 + operand2;
         ptr += 4;
         break;
       case 2:
         // Multiplication
-        instructions[instructions[ptr + 3]] = operand1 * operand2;
+        setValue(instructions, fullOpCode, ptr, 2, operand1 * operand2);
         ptr += 4;
         break;
       case 3:
         // Input
-        instructions[instructions[ptr + 1]] = inputs.shift();
+        setValue(instructions, fullOpCode, ptr, 0, inputs.shift());
         ptr += 2;
         break;
       case 4:
@@ -57,14 +66,19 @@ function* runIntMachine(instructionsParam, inputParams = []) {
         break;
       case 7:
         // It first operand is less than second
-        instructions[instructions[ptr + 3]] = operand1 < operand2 ? 1 : 0;
+        setValue(instructions, fullOpCode, ptr, 2, operand1 < operand2 ? 1 : 0);
         ptr += 4;
         break;
       case 8:
         // If operands are equal
-        instructions[instructions[ptr + 3]] = operand1 === operand2 ? 1 : 0;
+        setValue(instructions, fullOpCode, ptr, 2, operand1 === operand2 ? 1 : 0);
         ptr += 4;
         break;
+      case 9: {
+        relativeBase += getOperand(instructions, fullOpCode, ptr, 0);
+        ptr += 2;
+      }
+      break;
       case 99:
         ptr = Number.MAX_SAFE_INTEGER;
     }
