f6b7ebe72cf866c293d3cd1944f4b5dc6fb131ca
Ash Browning <ambrowning89@gmail.com>
2021-01-01 17:39:03 +0000

    refactor(day7): caching bag quantity values
    Could have gone recursive, but may not have worked for deeply
    nested bags

--- src/2020/07/part2.js
+++ src/2020/07/part2.js
@@ -1,6 +1,7 @@
 const lineRegex = /^(.*) bags contain (.*)/;
 const contentsRegex = /(\d+) ([^\d]*) bags?(,|\.)?/;
 const EMPTY_BAG_STR = 'no other bags.';
+const SHINY_GOLD = 'shiny gold';
 
 const solution = input => {
   const bags = input.reduce((acc, line) => {
@@ -19,19 +20,25 @@ const solution = input => {
     return acc;
   }, {});
 
-  const queue = ['shiny gold'];
-  let total = 0;
-  while (queue.length !== 0) {
-    const currentBag = queue.shift();
-    total += bags[currentBag].reduce((acc, bag) => {
-      for(let i = 0; i< bag.quantity; ++i) {
-        queue.push(bag.name);
-      }
-      acc += bag.quantity;
-      return acc;
+  const stack = [SHINY_GOLD];
+  const cache = {};
+  while (stack.length !== 0) {
+    const currentBag = stack[stack.length - 1];
+    const nonCachedBags = bags[currentBag].filter(bag => cache[bag.name] === undefined);
+
+    if (nonCachedBags.length !== 0) {
+      stack.push(...nonCachedBags.map(b => b.name));
+      continue;
+    }
+
+    // we have every bag cached!
+    cache[currentBag] = bags[currentBag].reduce((acc, bag) => {
+      return acc + (bag.quantity * cache[bag.name]) + bag.quantity;
     }, 0);
+
+    stack.pop();
   }
-  return total;
+  return cache[SHINY_GOLD];
 };
 
 const answer = 30899;
