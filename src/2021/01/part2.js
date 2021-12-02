1919a072aca09b786ca141ccf76fecc5eee65c15
Ash Browning <ambrowning89@gmail.com>
2021-12-02 19:52:52 +0000

    refactor(day01): using a little window size hack
    Only look at the elements leaving/entering the window to determine size
    value difference

--- src/2021/01/part2.js
+++ src/2021/01/part2.js
@@ -1,15 +1,12 @@
 const solution = input => {
   const parsedInput = input.map(i => +i);
 
-  let prev = Number.MAX_SAFE_INTEGER;
   let total = 0;
-  for (let i = 2; i < parsedInput.length; ++i) {
-    const window = parsedInput[i-2] + parsedInput[i-1] + parsedInput[i];
-    total += prev < window ? 1 : 0;
-    prev = window;
+  for (let i = 3; i < parsedInput.length; ++i) {
+    total += parsedInput[i - 3] < parsedInput[i] ? 1 : 0;
   }
 
-  return total
+  return total;
 };
 
 const answer = 1538;
