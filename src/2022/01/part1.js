4892c0a9fc092fef62a6fe040792350913d091f3
Ash Browning <ambrowning89@gmail.com>
2022-12-02 19:46:30 +0000

    feat(day1): optimise part 1

--- src/2022/01/part1.js
+++ src/2022/01/part1.js
@@ -2,14 +2,9 @@ const solution = input => {
   let currentBiggestElf = 0;
   let currentElf = 0;
   for (let calories of input) {
-    if (calories === '') {
-      if (currentBiggestElf < currentElf) {
-        currentBiggestElf = currentElf;
-      }
-      currentElf = 0;
-    } else {
-      currentElf += +calories;
-    }
+    currentElf = calories === '' ? 0 : currentElf + Number(calories);
+    currentBiggestElf =
+      currentBiggestElf < currentElf ? currentElf : currentBiggestElf;
   }
 
   return currentBiggestElf;
