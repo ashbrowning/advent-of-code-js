29e40808a7833b70cca97bd683747164c895e4d3
Ash Browning <ambrowning89@gmail.com>
2022-12-02 19:40:57 +0000

    chore(2022): update test/launcher to point to 2022

--- test/index.test.js
+++ test/index.test.js
@@ -5,22 +5,22 @@ import {
   getSolutionPath
 } from '../tools/pathResolvers';
 
-describe('Advent of Code 2019 JS', () => {
+describe('Advent of Code 2022 JS', () => {
   for (let i = 1; i <= 14; ++i) {
     describe(`Day ${i}`, () => {
       const day = dayResolver(i);
       const input = fs
-        .readFileSync(getInputPath({ day, year: 2019 }), { encoding: 'utf8' })
+        .readFileSync(getInputPath({ day, year: 2022 }), { encoding: 'utf8' })
         .split('\n');
       it('should give the correct answers for part 1', async () => {
         const { solution, answer } = await import(
-          getSolutionPath({ day, part: 1, year: 2019 })
+          getSolutionPath({ day, part: 1, year: 2022 })
         );
         expect(solution(input)).toBe(answer);
       });
       it('should give the correct answers for part 2', async () => {
         const { solution, answer } = await import(
-          getSolutionPath({ day, part: 2, year: 2019 })
+          getSolutionPath({ day, part: 2, year: 2022 })
         );
         expect(solution(input)).toBe(answer);
       });
