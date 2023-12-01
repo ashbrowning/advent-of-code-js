839e1b75fbdd62647040adf698b14360196f207d
Ash Browning <ambrowning89@gmail.com>
2023-12-01 08:26:30 +0000

    feat(2023): prepare for 2023 solutions

--- test/index.test.js
+++ test/index.test.js
@@ -5,22 +5,22 @@ import {
   getSolutionPath
 } from '../tools/pathResolvers';
 
-describe('Advent of Code 2022 JS', () => {
-  for (let i = 1; i <= 14; ++i) {
+describe('Advent of Code 2023 JS', () => {
+  for (let i = 1; i <= 1; ++i) {
     describe(`Day ${i}`, () => {
       const day = dayResolver(i);
       const input = fs
-        .readFileSync(getInputPath({ day, year: 2022 }), { encoding: 'utf8' })
+        .readFileSync(getInputPath({ day, year: 2023 }), { encoding: 'utf8' })
         .split('\n');
       it('should give the correct answers for part 1', async () => {
         const { solution, answer } = await import(
-          getSolutionPath({ day, part: 1, year: 2022 })
+          getSolutionPath({ day, part: 1, year: 2023 })
         );
         expect(solution(input)).toBe(answer);
       });
       it('should give the correct answers for part 2', async () => {
         const { solution, answer } = await import(
-          getSolutionPath({ day, part: 2, year: 2022 })
+          getSolutionPath({ day, part: 2, year: 2023 })
         );
         expect(solution(input)).toBe(answer);
       });
