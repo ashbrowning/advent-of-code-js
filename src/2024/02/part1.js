742d3aed3171e6f1909704712e29d1446f0831a2
Ash Browning <ambrowning89@gmail.com>
2024-12-15 17:23:41 +0000

    feat(day2): add part 1 solution

---
+++ src/2024/02/part1.js
@@ -0,0 +1,26 @@
+const parseInput = input => input.map(line => line.split(' ').map(x => parseInt(x)));
+
+const testReport = report => {
+  const differences = [];
+  for (let i = 0; i < report.length - 1; i++) {
+    differences.push(report[i + 1] - report[i]);
+  }
+
+  differences.sort((a, b) => a - b);
+
+  if (Math.abs(differences.at(0)) > 3 || Math.abs(differences.at(-1)) > 3) {
+    return false;
+  }
+
+  return differences.every(levelDelta => levelDelta > 0) || differences.every(levelDelta => levelDelta < 0);
+};
+
+const solution = input => {
+  const parsedInput = parseInput(input);
+  const safeReports = parsedInput.filter(testReport);
+  return safeReports.length;
+};
+
+const answer = 224;
+
+export { solution, answer };
