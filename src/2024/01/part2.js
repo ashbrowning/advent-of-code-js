ae72d745d8cf15cfca2217bfa81516dd5cc78cc9
Ash Browning <ambrowning89@gmail.com>
2024-12-15 16:35:58 +0000

    feat(day1): add part 2 solution

---
+++ src/2024/01/part2.js
@@ -0,0 +1,30 @@
+const regex = /(\d+)\w+(\d+)/g;
+
+const solution = input => {
+  const firstList = [];
+  const secondList = [];
+  for (let line of input) {
+    const [first, second] = line.split("   ").map((x) => parseInt(x));
+    firstList.push(first);
+    secondList.push(second);
+  }
+
+  const cache = {};
+
+  let total = 0;
+  for (let i of firstList) {
+    console.log('i', i);
+    if (!cache[i]) {
+      const linetotal = secondList.reduce((acc, curr) => curr === i ? acc + 1 : acc, 0);
+      cache[i] = linetotal * i;
+    }
+    total += cache[i];
+  }
+
+  return total;
+
+};
+
+const answer = 21328497;
+
+export { solution, answer };
