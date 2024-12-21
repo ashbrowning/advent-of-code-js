d7504d6617805ac21fe3af5177b823b4323db4dc
Ash Browning <ambrowning89@gmail.com>
2024-12-21 13:10:42 +0000

    refactor(day4): move to cleaner solution

--- src/2024/04/part1.js
+++ src/2024/04/part1.js
@@ -1,76 +1,57 @@
-const xmasRegex = /(XMAS)/g;
-const samxRegex = /(SAMX)/g;
+
+const checkVectors = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0]
+];
+
+const XMAS = 'XMAS';
+
+const check = (input, x, y) => {
+  const possbileXmas = [];
+  for (const [dx, dy] of checkVectors) {
+    const cells = [];
+    for (let delta = 0; delta < 4; delta++) {
+      cells.push(input?.[y + dy * delta]?.[x + dx * delta]);
+    }
+    possbileXmas.push(cells);
+  }
+
+  return possbileXmas.map(r => r.join('')).reduce((acc, str) => {
+    return str === XMAS ? acc + 1 : acc;
+  }, 0)
+}
+
+const getXCoords = (input) => {
+  const coords = [];
+
+  // Find all 'X's 
+  for (const rowIndex in input) {
+    for (const colIndex in input[rowIndex]) {
+      if (input[rowIndex][colIndex] === 'X') {
+        coords.push([Number.parseInt(colIndex), Number.parseInt(rowIndex)]);
+      }
+    }
+  }
+
+  return coords;
+}
 
 const solution = input => {
 
-  console.time('parse');
-  // get all rows/columns/diagonals as strings
-  // Horizontal
-  const lines = [...input];
-  // const lines = []
+  const coords = getXCoords(input);
 
-  // Vertical
-  for (let i = 0; i < input[0].length; i++) {
-    let line = "";
-    for (let j = 0; j < input.length; j++) {
-      line += input[j][i];
-    }
-    lines.push(line);
-  }
+  return coords.reduce((acc, coord) => {
+    const [x, y] = coord;
+    const r = check(input, x, y);
+    return acc + r;
+  }, 0);
 
-  // Diagonal |/
-  for (let y = 0; y < input.length; y++) {
-    let line = "";
-    for (let x = 0; x <= y; x++) {
-      line += input[y - x][x];
-    }
-    lines.push(line);
-  }
-
-  // Diagonal _/
-  for (let x = 1; x < input.length; x++) {
-    let line = "";
-
-    for (let y = input.length - 1; y - x >= 0; y--) {
-      line += input[y][x - y + input.length - 1];
-    }
-    lines.push(line);
-  }
-
-  // Diagonal \|
-  for (let x = 0; x < input.length; x++) {
-    let line = "";
-    for (let y = 0; x - y >= 0; y++) {
-      line += input[input.length - 1 - y][x - y];
-    }
-    lines.push(line);
-  }
-
-  // // Diagonal -\
-  for (let y = input.length - 2; y >= 0; y--) {
-    let line = "";
-
-    for (let x = 0; y - x >= 0; x++) {
-      line += input[y - x][input.length - x - 1];
-    }
-    lines.push(line);
-  }
-
-  // for (let line of lines) {
-  //   console.log(line);
-  // }
-
-  console.timeEnd('parse');
-  console.time('calc');
-  
-  const rval = lines.reduce((acc, line) => {
-    const regexMatches = line.match(xmasRegex)?.length || 0;
-    const smaxMatches = line.match(samxRegex)?.length || 0;
-    // console.log(line, regexMatches, smaxMatches);
-    return acc + regexMatches + smaxMatches;
-  }, 0)
-  console.timeEnd('calc');
-  return rval;
 };
 
 const answer = 2569;
